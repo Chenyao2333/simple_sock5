c05dccc4560daeeb099d71bf8c4106ec139aa99d
Chenyao233 <841144792@qq.com>
2015-11-28 22:57:37 +0800

    fix bug: when kernel's send queue is full the connection whould be closed

--- common.c
+++ common.c
@@ -1,7 +1,8 @@
 #include "common.h"
 
-#define BUFFSIZE 1024
+#define BUFFSIZE 4096
 #define XOR_ENC_KEY 169
+
 int set_non_blocking(int fd) {
     int flags = fcntl(fd, F_GETFL, 0);
     if (flags < 0) return flags;
@@ -46,19 +47,52 @@ void recv_send(int fd1, int fd2) {
         pthread_exit(NULL);
     }
 }
+
+int recv_buff(int fd, char buff[], int *buff_cnt) {
+    int n = enc_recv(fd, buff + (*buff_cnt), BUFFSIZE - (*buff_cnt), 0);
+    if (n >= 0) (*buff_cnt) += n;
+    fprintf(stdout, "recv %d bytes from fd=%d\n", n, fd);
+    return n;
+}
+
+int send_buff(int fd, char buff[], int *buff_cnt) {
+    int n = send(fd, buff, *buff_cnt, 0);
+    if (n >= 0) {
+        memcpy(buff, buff + n, (*buff_cnt) - n);
+        (*buff_cnt) -= n;
+    }
+    fprintf(stdout, "send %d bytes to fd=%d\n", n, fd);
+    return n;
+}
+
 void bd_forword(int fd1, int fd2) {
-    fd_set readset;
+    fd_set readset, writeset;
     int max_fd = (fd1 > fd2 ? fd1 : fd2) + 1;
 
     set_non_blocking(fd1);
     set_non_blocking(fd2);
 
+    char buff1[BUFFSIZE], buff2[BUFFSIZE];
+    int buff1_cnt = 0, buff2_cnt = 0;
+
     while (1) {
         FD_ZERO(&readset);
-        FD_SET(fd1, &readset);
-        FD_SET(fd2, &readset);
+        FD_ZERO(&writeset);
 
-        int n = select(max_fd, &readset, NULL, NULL, NULL);
+        if (buff1_cnt < BUFFSIZE) {
+            FD_SET(fd1, &readset);
+        }
+        if (buff2_cnt < BUFFSIZE) {
+            FD_SET(fd2, &readset);
+        }
+        if (buff1_cnt) {
+            FD_SET(fd2, &writeset);
+        }
+        if (buff2_cnt) {
+            FD_SET(fd1, &writeset);
+        }
+
+        int n = select(max_fd, &readset, &writeset, NULL, NULL);
         if (n < 0) {
             fprintf(stderr, "bd_forword: %s\n", strerror(errno));
             close(fd1); close(fd2);
@@ -66,10 +100,21 @@ void bd_forword(int fd1, int fd2) {
         }
 
         if (FD_ISSET(fd1, &readset)) {
-            recv_send(fd1, fd2);
+            if (recv_buff(fd1, buff1, &buff1_cnt) <= 0) goto ret;
         }
         if (FD_ISSET(fd2, &readset)) {
-            recv_send(fd2, fd1);
+            if (recv_buff(fd2, buff2, &buff2_cnt) <= 0) goto ret;
+        }
+        if (FD_ISSET(fd1, &writeset)) {
+            if (send_buff(fd1, buff2, &buff2_cnt) <= 0) goto ret;
+        }
+        if (FD_ISSET(fd2, &writeset)) {
+            if (send_buff(fd2, buff1, &buff1_cnt) <= 0) goto ret;
         }
     }
+ret:
+    fprintf(stdout, "forword be closed, err: %s\n", strerror(errno));
+    close(fd1);
+    close(fd2);
+    pthread_exit(NULL);
 }
